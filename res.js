f0996656413ee626b1c0f8fec549e287104ad0df
Deamon <dittopower@gmail.com>
2016-06-11 01:21:59 +1000

    v0.5

--- res.js
+++ res.js
@@ -1,5 +1,23 @@
+/*
+Resources - Files, Images, Sound, Data
+*/
+
 //files
 
 //audio
 audio_crit_sucess = "yay.m4a";
 audio_crit_failure = "fail.m4a";
+
+//Data
+Abilities = ["STR","DEX","CON","INT","WIS","CHA"];
+Skills = ["Acrobatics", "Appraise", "Bluff", "Climb", "Craft", "Diplomacy", "Disable Device", "Disguise", "Escape Artist", "Fly", "Handle Animal", "Heal", "Intimidate", "Knowledge Arcana", "Knowledge Dungeoneering", "Knowledge Engineering", "Knowledge Geography", "Knowledge History", "Knowledge Local", "Knowledge Nature", "Knowledge Nobility", "Knowledge Planes", "Knowledge Religion", "Linguistics", "Perception", "Perform", "Profession", "Ride", "Sense Motive", "Sleight of Hand", "Spellcraft", "Stealth", "Survival", "Swim", "Use Magic Device"];
+Sizes = [];
+Sizes.push(new Size("Fine",			8,	-8,	8,	16,		0.5,	0,	[0.5,">"],	[0.125,">"]));
+Sizes.push(new Size("Diminutive",	4,	-4,	6,	12,		1,		0,	[1,">"],	[1,">"]));
+Sizes.push(new Size("Tiny",			2,	-2,	4,	8,		2.5,	0,	[2,">"],	[8,">"]));
+Sizes.push(new Size("Small",		1,	-1,	2,	4,		5,		5,	[4,">"],	[60,">"]));
+Sizes.push(new Size("Medium",		0,	0,	0,	0,		5,		5,	[8,">"],	[500,">"]));
+Sizes.push(new Size("Large",		-1,	1,	-2,	-4,		10,		10,	[16,">"],	[4000,">"]));
+Sizes.push(new Size("Huge",			-2,	2,	-4,	-8,		15,		15,	[32,">"],	[32000,">"]));
+Sizes.push(new Size("Gargantuan",	-4,	4,	-6,	-12,	20,		20,	[64,">"],	[250000,">"]));
+Sizes.push(new Size("Colossal",		-8,	8,	-8,	-16,	30,		30,	[64,"<"],	[250000,"<"]));
--- ui.js
+++ ui.js
@@ -1,7 +1,14 @@
-statblock = $("#stat");
-logblock = $("#log");
-textblock = $("<div id='rolls'>");
-mainblock = $("#main");
+var menublock = $("<div id='menu' class='sections'>");
+var logblock = $("<div id='log' class='sections'>");
+var textblock = $("<div id='rolls'>");
+var mainblock = $("<div id='main' class='sections'>");
+
+var statblock = $("<div id='stat' class='mstats'>");
+var tabs_rolls = $("<div id='controls-rolls' class='stat mrolls'>");
+var tabs_main = $("<div id='controls-main' class='stat mmain'>");
+var tabs_chars = $("<div id='controls-char' class='stat mchars'>");
+
+var disChars = $("<select id=charsel>");
 var audio;
 
 characters = new Array();
@@ -9,55 +16,58 @@ current = "Default";
 characters[current] = new Character(current,'D_player',1,'Human','Fighter','N',10,10,10,10,10,10,"None","Australia",'Male',5.7,90,Sizes[4],'blond','green');
 
 function mytabs(){
-	var tabs = $("<div id='controls'>");
-	var tabui = $("<ul>");
-	tabs.append(tabui);
-	tabui.append("<li><a href='#controls-main'>Main</a></li>");
-	tabui.append("<li><a href='#controls-rolls'>Rolls</a></li>");
-	tabui.append("<li><a href='#controls-char' onclick=\"charsUpdate()\">Characters</a></li>");
-	tabs.append("<hr>");
+	disChars.appendTo(document.body);
+	menublock.appendTo(document.body);
+	mainblock.appendTo(document.body);
+	logblock.appendTo(document.body);
 	
-	//Main Tab
-	var tabs_cont = $("<div id='controls-main'>stuff</div>");
-	tabs.append(tabs_cont);
+//Menu
+	menublock.append("<h1>Menu</h1>");
+	menublock.append("<h4 class='mmain' onclick=\"reveal('main')\">Character Info</h4>");
+	menublock.append("<h4 class='mstats' onclick=\"reveal('stats')\">Abilities & Skills</h4>");
+	menublock.append("<h4 class='mrolls' onclick=\"reveal('rolls')\">Rolls</h4>");
+	menublock.append("<h4 class='mchars' onclick=\"reveal('chars')\">Characters</h4>");
 	
-	//Rolls Tabs
-	tabs_cont = $("<div id='controls-rolls'>");
-	tabs_cont.append("<h2>Abilities</h2>");
-	for(ib = 0; ib < Abilities.length;ib++){
-		tabs_cont.append("<button onclick='roll_abl(\""+Abilities[ib]+"\")'>"+Abilities[ib]+"</button>");
-	}
-	tabs_cont.append("<hr><h2>Skills</h2>");
-	for(ib = 0; ib < Skills.length;ib++){
-		tabs_cont.append("<button onclick='roll_skl(\""+Skills[ib]+"\")'>"+Skills[ib]+"</button>");
-	}
-	tabs_cont.append("<hr><h2>Offence</h2>");
-	tabs_cont.append("<button onclick='roll_CMB()'>CMB - Combat Maneuver</button>");
-	tabs_cont.append("<button onclick='roll_CMD()'>CMD - Combat Defence Maneuver</button>");
-	tabs_cont.append("<hr><h2>Defence</h2>");
-	tabs_cont.append("<button onclick='roll_save(\"fort\")'>Fortitude</button>");
-	tabs_cont.append("<button onclick='roll_save(\"refl\")'>Reflex</button>");
-	tabs_cont.append("<button onclick='roll_save(\"will\")'>Will</button>");
-	tabs.append(tabs_cont);
-	
-	//Characters Tab
-	tabs_cont = $("<div id='controls-char'>");
-	disChars = $("<select>");
 	disChars[0].onchange = function(eve){current=eve.target.value; displayStats();}
-	var disTemp = $("<h2>Select a Character:</h2>");
-	disTemp.append(disChars);
-	tabs_cont.append(disTemp);
-	tabs_cont.append("<hr><h2>Create a Character (CS)</h2>");
-	tabs_cont.append("<hr><h2>Level Up(CS)</h2>");
-	tabs.append(tabs_cont);
 	
-	mainblock.append(tabs);
-	$(function() {
-		$( "#controls" ).tabs();
-	  });
-	  
+//Main Tab
+	tabs_main.hide();
+	mainblock.append(tabs_main);
+	
+//Rolls Tab
+	tabs_rolls.hide();
+	tabs_rolls.append("<h2>Abilities</h2>");
+	for(ib = 0; ib < Abilities.length;ib++){
+		tabs_rolls.append("<button onclick='roll_abl(\""+Abilities[ib]+"\")'>"+Abilities[ib]+"</button>");
+	}
+	tabs_rolls.append("<hr><h2>Skills</h2>");
+	for(ib = 0; ib < Skills.length;ib++){
+		tabs_rolls.append("<button onclick='roll_skl(\""+Skills[ib]+"\")'>"+Skills[ib]+"</button>");
+	}
+	tabs_rolls.append("<hr><h2>Offence</h2>");
+	tabs_rolls.append("<button onclick='roll_CMB()'>CMB - Combat Maneuver</button>");
+	tabs_rolls.append("<button onclick='roll_CMD()'>CMD - Combat Defence Maneuver</button>");
+	tabs_rolls.append("<hr><h2>Defence</h2>");
+	tabs_rolls.append("<button onclick='roll_save(\"fort\")'>Fortitude</button>");
+	tabs_rolls.append("<button onclick='roll_save(\"refl\")'>Reflex</button>");
+	tabs_rolls.append("<button onclick='roll_save(\"will\")'>Will</button>");
+	mainblock.append(tabs_rolls);
+	
+//Characters Tab
+	tabs_chars.hide();
+	tabs_chars.append("<hr><h2>Create a Character (CS)</h2>");
+	tabs_chars.append("<hr><h2>Level Up(CS)</h2>");
+	mainblock.append(tabs_chars);
+
+//Stats Tab
+	statblock.hide();
+	mainblock.append(statblock);
+
+//Log
 	logblock.append("<h1>Log</h1>");
 	logblock.append(textblock);
+	reveal();
+	upkeep();
 }
 
 function charsUpdate(){
@@ -71,33 +81,61 @@ function charsUpdate(){
 	}
 }
 
+function reveal(data){
+	mainblock.children().hide();
+	mainblock.removeClass();
+	mainblock.addClass("sections");
+	switch(data){
+		case 'rolls':
+			tabs_rolls.show();
+			break;
+		case 'stats':
+			statblock.show();
+			break;
+		case 'chars':
+			tabs_chars.show();
+			break;
+		default:
+			tabs_main.show();
+			data = 'main';
+	}
+	mainblock.addClass("m"+data)
+}
+
 function displayStats(){
 	statblock.empty();
+	tabs_main.empty();
 	var disTemp;
-	statblock.append("<div class='name'>Name: "+ characters[current].Name +"</div>");
-	statblock.append("<div class='alignment'>Alignment: "+ characters[current].Alignment +"</div>");
-	statblock.append("<div class='player'>Player: "+ characters[current].Player +"</div>");
-	statblock.append("<div class='race'>Race: "+ characters[current].Race +"</div>");
-	statblock.append("<div class='class'>Class: "+ characters[current].Class +"</div>");
-	statblock.append("<div class='level'>Level: "+ characters[current].Level +"</div>");
-	statblock.append("<div class='deity'>Deity: "+ characters[current].Deity +"</div>");
-	statblock.append("<div class='homeland'>Homeland: "+ characters[current].Homeland +"</div>");
-	statblock.append("<div class='gender'>Gender: "+ characters[current].Gender +"</div>");
-	statblock.append("<div class='height'>Height: "+ characters[current].Height +"</div>");
-	statblock.append("<div class='weight'>Weight: "+ characters[current].Weight +"</div>");
-	statblock.append("<div class='hair'>Hair: "+ characters[current].Hair +"</div>");
-	statblock.append("<div class='eye'>Eye: "+ characters[current].Eye +"</div>");
-	//abilitys
-	disTemp = $("<table class='abilities'><tr><th>Ability</th><th>Score</th><th>Mod</th></tr></table>");
+
+//basic
+	tabs_main.append("<h1>Character Info</h1>");
+	tabs_main.append("<div class='name'>Name: "+ characters[current].Name +"</div>");
+	tabs_main.append("<div class='alignment'>Alignment: "+ characters[current].Alignment +"</div>");
+	tabs_main.append("<div class='player'>Player: "+ characters[current].Player +"</div>");
+	tabs_main.append("<div class='race'>Race: "+ characters[current].Race +"</div>");
+	tabs_main.append("<div class='class'>Class: "+ characters[current].Class +"</div>");
+	tabs_main.append("<div class='level'>Level: "+ characters[current].Level +"</div>");
+	tabs_main.append("<div class='deity'>Deity: "+ characters[current].Deity +"</div>");
+	tabs_main.append("<div class='homeland'>Homeland: "+ characters[current].Homeland +"</div>");
+	tabs_main.append("<div class='gender'>Gender: "+ characters[current].Gender +"</div>");
+	tabs_main.append("<div class='height'>Height: "+ characters[current].Height +"</div>");
+	tabs_main.append("<div class='weight'>Weight: "+ characters[current].Weight +"</div>");
+	tabs_main.append("<div class='hair'>Hair: "+ characters[current].Hair +"</div>");
+	tabs_main.append("<div class='eye'>Eye: "+ characters[current].Eye +"</div>");
+
+	statblock.append("<h1>Abilities & Skills</h1>");
+//abilitys
+	disTemp = $("<table class='abilities'><tr><th></th><th>Ability</th><th>Score</th><th>Mod</th></tr></table>");
 	for(ib = 0; ib < Abilities.length;ib++){
 		disTempRow = $("<tr>");
+		disTempRow.append("<td><div class='rollable' onclick='roll_abl(\""+Abilities[ib]+"\")'>x</div></td>");
 		disTempRow.append("<td>"+Abilities[ib]+"</td>");
 		disTempRow.append("<td>"+characters[current].ability_score(Abilities[ib])+"</td>");
 		disTempRow.append("<td>"+characters[current].ability_mod(Abilities[ib])+"</td>");
 		disTemp.append(disTempRow);
 	}
 	statblock.append(disTemp);
-	//skills
+//skills
 	disTemp = $("<table class='skills'><tr><th>*</th><th>Skills</th><th>Total</th><th>Mod</th><th>Ranks</th><th>Misc</th></tr></table>");
 	for(ib = 0; ib < Skills.length;ib++){
 		disTempRow = $("<tr>");
@@ -127,16 +165,7 @@ function displayStats(){
 	}
 	statblock.append(disTemp);
 	
-	statblock.append("<div class='ac'>AC: "+ characters[current].ac() +" Flat: "+ characters[current].ac(true) +"</div>");
-	statblock.append("<div class='actouch'>AC Touch: "+ characters[current].ac_touch() +" Flat: "+ characters[current].ac_touch(true) +"</div>");
-	
-	statblock.append("<div class='fort'>Fortitude: "+ characters[current].save_fort() +"</div>");
-	statblock.append("<div class='refl'>Reflex: "+ characters[current].save_refl() +"</div>");
-	statblock.append("<div class='will'>Will: "+ characters[current].save_will() +"</div>");
-	
-	statblock.append("<div class='bab'>Attack Bonus: "+ characters[current].BAB() +"</div>");
-	statblock.append("<div class='cmb'>CMB: "+ characters[current].CMB() +"</div>");
-	statblock.append("<div class='cmd'>CMD: "+ characters[current].CMD() +"</div>");
+//combat
 }
 
 function chat_msg(msg,clas){
@@ -152,12 +181,21 @@ function sound_play(sound){
 	audio.play();
 }
 
+function upkeep(){
+	displayStats();
+	var il = 0; var data;
+	for(data in characters){il++;}
+	if(disChars.children().length != il){
+		charsUpdate();
+	}
+}
+
 onload = function(){
-	statblock = $("#stat");
-	logblock = $("#log");
-	textblock = $("<div id='rolls'>");
-	mainblock = $("#main");
 	mytabs();
 	audio = new Audio();
-	tick = setInterval(function(){displayStats();}, 1000);
+	tick = setInterval(function(){upkeep();}, 10000);
 }
+
+
+
+
